dd4971f698d86013521328ba573cd2ba27c516af
rjumamuradov <rozymyrat.jumamuradov@gmail.com>
2022-01-06 18:08:02 +0100

    lambda

--- src/lambda/Lambda01.java
+++ src/lambda/Lambda01.java
@@ -149,6 +149,7 @@ public class Lambda01 {
         }
         Math.random();
 
+
     }
 
 
--- src/lambda/Test3.java
+++ src/lambda/Test3.java
@@ -3,6 +3,10 @@ package lambda;
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("");
+        int r=0;
+        int t=1;
+        System.out.println(r/t);
+        System.out.println(t/r);
 
 
 
